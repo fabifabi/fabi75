069f95123c794ff4e4a51fb9b84435134d542f20
fabi75 <minteo1980@gmail.com>
2020-10-15 23:06:58 +0200

    add meta

---
+++ public/tools/meta.js
@@ -0,0 +1,29 @@
+//   all(window);
+//don't work on array
+function all(obj, tabexception, funproxy, propchange, propread) {
+    //        log(typeof obj + "--------------run");
+    for (var i in obj) {
+        if (tabexception.indexOf(i) > -1)
+            continue;
+        if (Array.isArray(obj[i]))
+            continue;
+        log(i + " : ", typeof obj[i]);
+        if (typeof obj[i] === "object") {
+            all(obj[i], tabexception, funproxy, propproxy);
+            continue;
+        }
+        if (typeof obj[i] === "function") {
+            obj[i] = funproxy(i, obj[i]);
+            continue;
+        }
+        if (typeof obj[i] === "function") {
+            Object.defineProperty(obj, i, {
+                set: function (newval) { propchange(i, obj[i], newval); },
+                get: function () { return propread(i, obj[i]); }
+            });
+            continue;
+        }
+
+    }
+}
+
