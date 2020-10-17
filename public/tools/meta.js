886de2360cd6b1c832b4121275794cbc5b556f1e
fabi75 <minteo1980@gmail.com>
2020-10-17 15:35:44 +0200

    catch

--- public/tools/meta.js
+++ public/tools/meta.js
@@ -3,23 +3,29 @@
 function all(obj, tabexception, funproxy, propchange, propread) {
     //        log(typeof obj + "--------------run");
     for (var i in obj) {
-        if (tabexception.indexOf(i) > -1)
-            continue;
-        if (Array.isArray(obj[i]))
-            continue;
-        log(i + " : ", typeof obj[i]);
-        if (typeof obj[i] === "object") {
-            all(obj[i], tabexception, funproxy, propchange, propread);
-            continue;
+        try {
+            if (tabexception.indexOf(i) > -1)
+                continue;
+            if (Array.isArray(obj[i]))
+                continue;
+            log(i + " : ", typeof obj[i]);
+            if (typeof obj[i] === "object") {
+                all(obj[i], tabexception, funproxy, propchange, propread);
+                continue;
+            }
+            if (typeof obj[i] === "function") {
+                obj[i] = funproxy(i, obj[i]);
+                continue;
+            }
+            Object.defineProperty(obj, i, {
+                set: function (newval) { propchange(i, obj[i], newval); },
+                get: function () { return propread(i, obj[i]); }
+            });
         }
-        if (typeof obj[i] === "function") {
-            obj[i] = funproxy(i, obj[i]);
-            continue;
+        catch (e) {
+            log(e);
         }
-        Object.defineProperty(obj, i, {
-            set: function (newval) { propchange(i, obj[i], newval); },
-            get: function () { return propread(i, obj[i]); }
-        });
+
     }
 }
 
