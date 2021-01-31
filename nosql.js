b9e828643ca8eec44a0414853f2f4b7aba4da6f5
fabifabi <minteo1980@gmail.com>
2021-01-31 12:54:23 +0000

    make it better

---
+++ nosql.js
@@ -0,0 +1,80 @@
+// @ts-check
+/*
+const { ObjectId } = require('mongodb');
+
+ObjectId.prototype.toString = ObjectId.prototype.toHexString;
+var log = console.log;
+
+function sendMail(email) {
+    var key = "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"
+    const mail = require("@sendgrid/mail");
+    mail.setApiKey(key)
+
+    const msg = {
+        to: email,
+        from: 'fabi75.pro@gmail.com', // Use the email address or domain you verified above
+        subject: 'Sending with Twilio SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+    mail
+        .send(msg)
+        .then(() => { }, error => {
+            log(error);
+            if (error.response) {
+                log(error.response.body)
+            }
+        });
+}*/
+
+let allDB;
+
+async function write(all) {
+    await allDB.insertOne({ name: "all", data: JSON.stringify(all) });
+}
+
+var all = {
+    tab: {},
+    resto: {}
+};
+
+
+async function get() {
+    var all2 = await allDB.findOne({ name: "all" });
+    log(all2)
+    if (all2 !== null)
+        all = JSON.parse(all2.data);
+    return all;
+}
+
+
+exports.getAll = get;
+exports.write = write;
+
+async function connect() {
+    const MongoClient = require('mongodb').MongoClient;
+    const uri = "mongodb+srv://FabiRoot:FabiRoot1003@cluster0.3or7k.mongodb.net/Trad?retryWrites=true&w=majority";
+    var client = await MongoClient.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+    var fullDB = client.db("lisboa");
+    allDB = fullDB.collection("resto");
+    get();
+}
+
+async function clean() {
+    await allDB.write({});
+}
+
+var log = console.log;
+
+async function initDB() {
+    try {
+        await connect();
+        log("fin init db");
+    } catch (e) { log(e) }
+}
+
+// liste plat du jour pour tout les telephone associé?
+//humhum
+// tel=nom de resto
+
+initDB();
