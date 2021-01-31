198d2eb2e5bb80982c0c3ee7fc705108498009fc
fabifabi <minteo1980@gmail.com>
2021-01-31 12:32:36 +0000

    make it better

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var http = require('http').createServer(app);
 var anchorme = require("anchorme").default;
 var emoji = require('node-emoji');
 
-var getAll = require('./nosql').get;
-var write = require('./nosql').write;
+var getAll = require('./nosql').getAll;
+var writeAll = require('./nosql').write;
 
 var log = console.log;
 
@@ -13,36 +13,61 @@ app.use(express.static(__dirname + "/public"));
 
 app.set('port', process.env.PORT || 3000);
 
-var tab = [];
-var resto = {};
+
+var all = {
+  tab: {},
+  resto: {}
+};
 
 app.get('/lisboa', function (req, res) {
+  getAll();
   log(req.query)
   var obj = JSON.parse(req.query.res);
   if (obj.body.indexOf("create ") === 0) {
-    var all = obj.body.split(" ")
-    all.unshift();
+    var body = obj.body.split(" ")
+    body.unshift();
     var nom = all.join("")
-    resto[obj.address] = nom;
+    all.resto[obj.address] = nom;
+    writeAll(all);
   }
   else {
-    if (resto[obj.address])
-      tab.push({ from: obj.address, txt: obj.body, at: new Date(obj.date) })
+    if (all.resto[obj.address]) {
+      var at = new Date(obj.date);
+      var key = at.getFullYear * 10000 + at.getMonth * 100 + at.getDay;
+      if (all.tab[key] === undefined)
+        all.tab[key] = []
+      all.tab[key].push({ from: all.resto[obj.address], txt: obj.body.split(".") });
+      writeAll(all)
+    }
   }
   res.send(req.query)
 })
 
 app.get('/clean', function (req, res) {
   log(req.query)
-  tab = [];
+  var all = {
+    tab: {},
+    resto: {}
+  }
+  writeAll(all)
   res.send(req.query)
 })
 
 app.get('/menu', function (req, res) {
-  var txt = "";
-  for (var i = 0; i < tab.length; i++) {
-    var l = tab[i];
-    txt += "From : " + l.from + " at " + l.at.toLocateDate() + " say :" + l.txt + "<br>";
+  var at = Date.now();
+  var key = at.getFullYear * 10000 + at.getMonth * 100 + at.getDay;
+  all = getAll();
+  var txt = "<div class='title'>Today : </div><br>";
+  if (all.tab[key] !== undefined) {
+    for (var i = 0; i < all.tab[key].length; i++) {
+      var l = all.tab[key][i];
+      txt += "<div class='Name'>" + l.from + "</div><br>"
+      for (var j = 0; j < l.txt.length; j++) {
+        txt += "<div class='Plat'>" + l.txt[j] + " : " + l.txt[j + 1] + "</div><br>";
+        j++;
+      }
+      l.txt + "<br>";
+    }
   }
   res.send(txt);
 })
