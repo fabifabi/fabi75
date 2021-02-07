81d891217c051c74b1a9e7b8e02024c9c6a4f1e6
fabifabi <minteo1980@gmail.com>
2021-02-07 15:39:37 +0000

    make it better

--- outlook.js
+++ outlook.js
@@ -13,35 +13,50 @@ const client = new Client({
 }
 );
 
+var all = [];
+var lastTry = undefined;
+
 var log = console.log;
 async function searchMail() {
-    var all = [];//[{resto:"",foods:[""],photo:[""]}]}];
-    await client.connect();
-    const messages = await client.retrieveAll();
-    var date = new Date();
-    var datestr = date.toISOString();
-    log(datestr.substr(0, 10));
-    messages.forEach((message) => {
-        var unresto = {}
-        log(typeof message.date);
-        console.log(message.subject);
-        unresto.date = message.date.toISOString();
-        if (unresto.date.indexOf(datestr) !== -1)
-            return;
-        unresto.resto = message.subject;
-        console.log(message.text)
-        unresto.foods = message.text.split("/n");
-        unresto.photo = [];
-        for (var i = 0; i < message.attachments.length; i++) {
-            //    log(message.attachments[i])
-            var ext = message.attachments[i].fileName.substr(message.attachments[i].fileName.lastIndexOf(".") + 1);
-            log(ext)
-            var src = "data:image/" + ext + ";base64," + message.attachments[i].content.toString("base64");
-            unresto.photo.push(src);
+    try {
+        var date = new Date();
+        if (lastTry !== undefined) {
+            var diff = lastTry - date;
+            if (diff < 1000 * 60 * 10)
+                return;
         }
-        all.push(unresto);
-    });
-    await client.quit();
+        lastTry = date;
+        all = [];//[{resto:"",foods:[""],photo:[""]}]}];
+        await client.connect();
+        const messages = await client.retrieveAll();
+
+        var datestr = date.toISOString();
+        log(datestr.substr(0, 10));
+        messages.forEach((message) => {
+            var unresto = {}
+            log(typeof message.date);
+            console.log(message.subject);
+            unresto.date = message.date.toISOString();
+            if (unresto.date.indexOf(datestr) !== -1)
+                return;
+            unresto.resto = message.subject;
+            console.log(message.text)
+            unresto.foods = message.text.split("/n");
+            unresto.photo = [];
+            for (var i = 0; i < message.attachments.length; i++) {
+                //    log(message.attachments[i])
+                var ext = message.attachments[i].fileName.substr(message.attachments[i].fileName.lastIndexOf(".") + 1);
+                log(ext)
+                var src = "data:image/" + ext + ";base64," + message.attachments[i].content.toString("base64");
+                unresto.photo.push(src);
+            }
+            all.push(unresto);
+        });
+        await client.quit();
+    } catch (e) {
+        log(e);
+        all = [];
+    }
     log(all);
     return all;
 };
