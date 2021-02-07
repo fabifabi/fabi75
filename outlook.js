ed40b37dcad98264662d2f3c9eda697560cddc1d
fabifabi <minteo1980@gmail.com>
2021-02-07 16:05:54 +0000

    make it better

--- outlook.js
+++ outlook.js
@@ -48,6 +48,7 @@ async function searchMail() {
                 //    log(message.attachments[i])
                 var ext = message.attachments[i].fileName.substr(message.attachments[i].fileName.lastIndexOf(".") + 1);
                 log(ext)
+                if (ext === "jpg") ext = "jpeg";
                 var src = "data:image/" + ext + ";base64," + message.attachments[i].content.toString("base64");
                 unresto.photo.push(src);
             }
