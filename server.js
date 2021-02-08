a6b9090c738229b9130281931804ee5dba6e019a
fabifabi <minteo1980@gmail.com>
2021-02-08 11:11:21 +0000

    make it better

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.get('/clean', function (req, res) {
 var log = console.log;
 
 app.get('/menu', function (req, res) {
-  console.log("header-----", JSON.stringify(req));
+  //console.log("header-----", JSON.stringify(req));
   async function a() {
 
     var at = new Date(Date.now());
