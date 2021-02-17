bb087338614f8b159187911349a7857fedea6403
fabifabi <minteo1980@gmail.com>
2021-02-17 11:19:01 +0000

    make it better

--- public/trad/main.js
+++ public/trad/main.js
@@ -13,6 +13,10 @@ var text = "Bonjour ! comment ca va ?";
 var urlFr2pt = 'https://www.googleapis.com/language/translate/v2?target=pt&source=fr&format=text&key=' + api + '&q=';
 var urlPt2fr = 'https://www.googleapis.com/language/translate/v2?target=fr&source=pt&format=text&key=' + api + '&q=';
 
+var url2fr = 'https://www.googleapis.com/language/translate/v2?target=fr&format=text&key=' + api + '&q=';
+
+var urlDetect = "https://translation.googleapis.com/language/translate/v2/detect&key=" + api + '&q='
+
 var hop, hop2;
 
 function change(src, urlsrc, urldst) {
@@ -21,38 +25,47 @@ function change(src, urlsrc, urldst) {
     clearTimeout(hop2);
     hop = setTimeout(
         get
-        , 1000);
+        , 500);
     function get() {
-        getAjax(urlsrc + enc, function (res) {
+        getAjax(urlDetect + enc, function (res) {
             var txt = JSON.parse(res);
-            log(txt);
-            var val = txt.data.translations[0].translatedText
-            trad.value = val;
-            var enc = encodeURI(trad.value);
-            trad.select();
-            trad.setSelectionRange(0, 99999); /* For mobile devices */
-            document.execCommand("copy");
-            src.focus();
-            hop = setTimeout(
-                get2
-                , 2000);
-            function get2() {
-                getAjax(urldst + enc, function (res) {
-                    var txt = JSON.parse(res);
-                    log(txt);
-                    var val = txt.data.translations[0].translatedText
-                    trad2.value = val;
-                })
+            var lang = txt.data.detections.language;
+            var urls = urlFr2pt;
+            var urld = urlPt2fr;
+            if (lang === "pt") {
+                urld = urlFr2pt;
+                urls = urlPt2fr;
             }
-        })
-    }
+            getAjax(urls + enc, function (res) {
+                var txt = JSON.parse(res);
+                log(txt);
+                var val = txt.data.translations[0].translatedText
+                trad.value = val;
+                var enc = encodeURI(trad.value);
+                trad.select();
+                trad.setSelectionRange(0, 99999); /* For mobile devices */
+                document.execCommand("copy");
+                src.focus();
+                hop = setTimeout(
+                    get2
+                    , 500);
+                function get2() {
+                    getAjax(urld + enc, function (res) {
+                        var txt = JSON.parse(res);
+                        log(txt);
+                        var val = txt.data.translations[0].translatedText
+                        trad2.value = val;
+                    })
+                }
+            })
+        }
 }
 
-function changeFr() {
-    change(fr, urlFr2pt, urlPt2fr)
-}
-function changePt() {
-    change(pt, urlPt2fr, urlFr2pt)
-}
-window.onload = openFullscreen;
+    function changeFr() {
+        change(fr, urlFr2pt, urlPt2fr)
+    }
+    function changePt() {
+        change(pt, urlPt2fr, urlFr2pt)
+    }
+    window.onload = openFullscreen;
 //setTimeout("change()", 4000);
